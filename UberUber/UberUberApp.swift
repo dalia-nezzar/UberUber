5f14ea156177d550aab37ad1ffe99a9b862b6ada
Dalia <nezzar.dalia@gmail.com>
2024-12-11 16:24:20 +0100

    Initial Commit

---
+++ UberUber/UberUberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberUberApp.swift
+//  UberUber
+//
+//  Created by nezzar dalia on 11/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct UberUberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
